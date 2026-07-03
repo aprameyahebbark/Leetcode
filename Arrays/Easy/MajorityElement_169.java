c5c29654137eb969d27b011b29f3730cf22539ae
aprameyahebbark <aprameyahebbark@gmail.com>
2026-07-03 21:04:05 +0530

    Solved Majority Element

---
+++ Arrays/Easy/MajorityElement_169.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+public class MajorityElement_169{
+
+    public static int majorityElement(int[] nums) {
+
+        for (int i = 0; i < nums.length; i++) {
+
+            int count = 0;
+
+            for (int j = 0; j < nums.length; j++) {
+
+                if (nums[i] == nums[j]) {
+                    count++;
+                }
+            }
+
+            if (count > nums.length / 2) {
+                return nums[i];
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 2, 1, 1, 1, 2, 2};
+
+        // Call the method and store result
+        int result = majorityElement(nums);
+
+        System.out.println("Majority Element: " + result);
+    }
+}
