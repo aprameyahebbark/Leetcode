fabbebacbb71d7c01f41222bedb6f238a13d339d
aprameyahebbark <aprameyahebbark@gmail.com>
2026-07-07 17:25:09 +0530

    Prints True if Duplicate is present

---
+++ Arrays/Easy/ContainsDuplicate_217.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class ContainsDuplicate_217 {
+
+    public static boolean containsDuplicate(int[] nums) {
+
+        Arrays.sort(nums);
+
+        for (int i = 1; i < nums.length; i++) {
+
+            if (nums[i] == nums[i - 1]) {
+                return true;
+            }
+
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums1 = {1, 2, 3, 1};
+        int[] nums2 = {1, 2, 3, 4};
+        int[] nums3 = {1, 1, 1, 3, 3, 4, 3, 2, 4, 2};
+
+        System.out.println("Ex1: " + containsDuplicate(nums1));
+        System.out.println("Ex2: " + containsDuplicate(nums2));
+        System.out.println("Ex3: " + containsDuplicate(nums3));
+    }
+}
