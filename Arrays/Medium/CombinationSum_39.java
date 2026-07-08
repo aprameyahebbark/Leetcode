8cd217230487640e6074f88ee99ff040e208c618
aprameyahebbark <aprameyahebbark@gmail.com>
2026-07-08 16:35:04 +0530

    Combination Sum Problem Sloved using backtracking

---
+++ Arrays/Medium/CombinationSum_39.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+public class CombinationSum_39 {
+
+    public static void backtrack(int[] candidates,
+                                 int target,
+                                 int index,
+                                 List<Integer> current,
+                                 List<List<Integer>> result) {
+
+        if (target == 0) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+
+        if (target < 0) {
+            return;
+        }
+
+        for (int i = index; i < candidates.length; i++) {
+
+            current.add(candidates[i]);
+
+            backtrack(candidates,
+                    target - candidates[i],
+                    i,
+                    current,
+                    result);
+
+            current.remove(current.size() - 1);
+        }
+    }
+
+    public static List<List<Integer>> combinationSum(int[] candidates, int target) {
+
+        List<List<Integer>> result = new ArrayList<>();
+
+        backtrack(candidates, target, 0, new ArrayList<>(), result);
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        int[] candidates = {2, 3, 4, 6, 7};
+        int target = 7;
+
+        List<List<Integer>> answer = combinationSum(candidates, target);
+
+        System.out.println(answer);
+    }
+}
