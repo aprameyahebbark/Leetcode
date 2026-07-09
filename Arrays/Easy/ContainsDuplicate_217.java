8d1c06ee6c7847de465babf38e04749b7cd91bd6
aprameyahebbark <aprameyahebbark@gmail.com>
2026-07-09 12:18:56 +0530

    Add Fibonacci recursion solution

--- Arrays/Easy/ContainsDuplicate_217.java
+++ Arrays/Easy/ContainsDuplicate_217.java
@@ -22,9 +22,11 @@ public class ContainsDuplicate_217 {
         int[] nums1 = {1, 2, 3, 1};
         int[] nums2 = {1, 2, 3, 4};
         int[] nums3 = {1, 1, 1, 3, 3, 4, 3, 2, 4, 2};
+        int[] nums4= {5,7,8,4,1};
 
         System.out.println("Ex1: " + containsDuplicate(nums1));
         System.out.println("Ex2: " + containsDuplicate(nums2));
         System.out.println("Ex3: " + containsDuplicate(nums3));
+        System.out.println("Ex4: " + containsDuplicate(nums4));
     }
 }
---
+++ Recursion/Easy/FibonacciNumber_509.java
@@ -0,0 +1,24 @@
+package Easy;
+
+public class FibonacciNumber_509 {
+
+    public static int fib(int n) {
+
+        if (n == 0) {
+            return 0;
+        }
+        else if (n == 1) {
+            return 1;
+        }
+        else {
+            return fib(n - 1) + fib(n - 2);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int n = 9;
+
+        System.out.println("Fibonacci Number = " + fib(n));
+    }
+}
