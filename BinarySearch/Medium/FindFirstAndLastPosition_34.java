bb4d6b38ec85b4e7e60338856185fdcc07c1a522
aprameyahebbark <aprameyahebbark@gmail.com>
2026-07-29 20:08:06 +0530

    Find first and last position by binary search

---
+++ BinarySearch/Medium/FindFirstAndLastPosition_34.java
@@ -0,0 +1,61 @@
+package Medium;
+import java.util.Arrays;
+public class FindFirstAndLastPosition_34 {
+    public int[] searchRange(int[] nums, int target) {
+        int first = findFirst(nums, target);
+        int last = findLast(nums, target);
+        return new int[]{first, last};
+    }
+
+    private int findFirst(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        int ans = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                ans = mid;
+                right = mid - 1;
+            }
+            else if (nums[mid] < target) {
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
+        }
+        return ans;
+    }
+    private int findLast(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        int ans = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                ans = mid;
+                left = mid + 1;
+            }
+            else if (nums[mid] < target) {
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+
+        FindFirstAndLastPosition_34 obj = new FindFirstAndLastPosition_34();
+        int[] nums = {5, 7, 7, 8, 8, 10};
+        int target = 8;
+        int[] result = obj.searchRange(nums, target);
+        System.out.println(Arrays.toString(result));
+
+
+    }
+
+
+}
